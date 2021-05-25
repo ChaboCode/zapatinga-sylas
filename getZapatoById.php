b1e7735083a2178e79735e81e74703d7bcd46468
ChaboCode <kaerdos.dev@gmail.com>
2021-05-25 09:41:50 -0500

    dde3c14: Replace 2 SQL queries with JOIN

--- getZapatoById.php
+++ getZapatoById.php
@@ -17,8 +17,12 @@ if ($con->connect_error) {
 }
 
 // Setp 3. Query
-$sql = "SELECT * FROM zapatos WHERE id = ".$zapato_id;
+$sql = "SELECT id, image, price, model, seasons.season_name AS season FROM zapatos INNER JOIN seasons ON season = seasons.season_id WHERE id = ".$zapato_id;
 $result = $con->query($sql);
+
+if (!$result) {
+   die($con->error);
+}
 
 // Step 4. Generate and return results
 // Since only one row is returned, it can be directly echoed
