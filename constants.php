12fb84c0045e98d64b4958bfd1b8ba1690221d1c
ChaboCode <kaerdos.dev@gmail.com>
2021-05-24 22:52:08 -0500

    weird database name

--- constants.php
+++ constants.php
@@ -2,6 +2,6 @@
 $servername = "us-cdbr-east-03.cleardb.com";
 $username = 'bc95ede3cf856e';
 $password = '677ef378';
-$db = 'zapatinga';
+$db = 'heroku_fc2a50b646c7d66';
 $jwt_secret = 'Sw0rdf1sh-02cht34712348';
 ?>
