2d38ac5c8d07fa2a696dbebccad124136e21460b
ChaboCode <kaerdos.dev@gmail.com>
2021-05-24 22:32:55 -0500

    Updated database credentials

--- constants.php
+++ constants.php
@@ -1,9 +1,7 @@
 <?php
-$servername = 'localhost';
-$username = 'zapatinga';
-$password = 'zapatinga';
-$register_username = 'zapatinga_register';
-$register_password = '#L V3#L)Z MURC!#LAGO H!NDU C)M!A F#L!Z CARD*|238';
+$servername = "us-cdbr-east-03.cleardb.com"
+$username = 'bc95ede3cf856e';
+$password = '677ef378';
 $db = 'zapatinga';
 $jwt_secret = 'Sw0rdf1sh-02cht34712348';
 ?>
